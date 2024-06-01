82f38679c319f7cff6e17f18cf514c460b8b42fa
RaviRaghuv <ravikumarswn5@gmail.com>
2024-06-01 10:51:47 +0530

    Init

---
+++ src/Form.js
@@ -0,0 +1,29 @@
+// Export the Form component and also import the name and email from HomePage component
+import React from "react";
+import name from "./HomePage";
+import email from "./HomePage";
+
+
+function handleSubmit(e) {
+  e.preventDefault();
+ 
+ 
+}
+
+const Form = () => (
+  <>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <h3>Login Page</h3>
+        <input type="text" placeholder="Name" value={name}  />
+        <br />
+        <br />
+        <input type="email" placeholder="Email" value={email} />
+        <br />
+        <br />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  </>
+);
+export default Form;
